1b293de9117c1fb5500f62daf5f8da3929481b8a
rakesh reddy <rishiredd789@gmail.com>
2021-01-29 21:34:56 +0530

    Update test_proxies.py

--- test_proxies.py
+++ test_proxies.py
@@ -1,6 +1,6 @@
 '''
-Date: 3/15/2019
-Author: Mohamed
+Date: 29/01/2021
+Author: SKAR
 Description: Reads a file that contains a list of proxies and determines whether or not that list is good.
              Each line in the file must be in the format of ip:port
 '''
